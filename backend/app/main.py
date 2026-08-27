95997065068d8f136aaaddbd119835b21a909b16
Moises <moiseslugo9134@gmail.com>
2026-08-27 06:28:58 -0300

    fix:configure cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://www.moiseslugo.com/"],
+    allow_origins=["https://www.moiseslugo.com","http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
