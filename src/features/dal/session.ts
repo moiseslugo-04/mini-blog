35bddcdc2ea28ce6b3e69a613d37db548d79c0c2
Moises Lugo <105566569+moiseslugo-04@users.noreply.github.com>
2026-09-02 13:03:16 -0300

    Refactor verifySession to use credentials option

--- src/features/dal/session.ts
+++ src/features/dal/session.ts
@@ -1,19 +1,14 @@
 import 'server-only'
-import { cookies } from 'next/headers'
 import { API_URL } from '@/app/config/env'
 type User = { id: string; name: string; email: string; username: string }
 type AuthResponse = { isAuth: false; user: null } | { isAuth: true; user: User }
 export const verifySession = async (): Promise<AuthResponse> => {
-  const cookiesStore = await cookies()
-  const accessToken = cookiesStore.get('access_token')?.value
-  if (!accessToken) return { isAuth: false, user: null }
-
-  const response = await fetch(API_URL + '/users/me', {
-    headers: {
-      Cookie: `access_token=${accessToken}`,
-    },
-  })
+  
+  const response = await fetch(API_URL + '/users/me',{credentials: "include"})
+  
   if (!response.ok) return { isAuth: false, user: null }
+  
   const user = (await response.json()) as User
+  
   return { isAuth: true, user }
 }
