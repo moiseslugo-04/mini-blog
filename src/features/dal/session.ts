b80e073a69695b4ce33c5270b909349e6dbbf5ef
Moises Lugo <105566569+moiseslugo-04@users.noreply.github.com>
2026-09-02 13:41:17 -0300

    Enhance session verification with access token
    Updated session verification to include access token from cookies in the request headers.

--- src/features/dal/session.ts
+++ src/features/dal/session.ts
@@ -1,14 +1,22 @@
 import 'server-only'
+import { cookies } from 'next/headers'
 import { API_URL } from '@/app/config/env'
 type User = { id: string; name: string; email: string; username: string }
 type AuthResponse = { isAuth: false; user: null } | { isAuth: true; user: User }
 export const verifySession = async (): Promise<AuthResponse> => {
-  
-  const response = await fetch(API_URL + '/users/me',{credentials: "include"})
-  
+  const cookieStore = await cookies()
+  const accessToken = cookieStore.get('access_token')
+
+  console.log(accessToken, 'accessToken')
+  const response = await fetch(API_URL + '/users/me', {
+    credentials: 'include',
+    headers: {
+      Cookie: `access_token=${accessToken?.value ?? ''}`,
+    },
+  })
+
+  console.log(response, 'session')
   if (!response.ok) return { isAuth: false, user: null }
-  
   const user = (await response.json()) as User
-  
   return { isAuth: true, user }
 }
