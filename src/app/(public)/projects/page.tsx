3b39e7fb0c57e190bbf26a20d8d523cbef0bd58e
Moises Lugo <105566569+moiseslugo-04@users.noreply.github.com>
2026-09-03 10:14:00 -0300

    Translate project page content to Portuguese
    Updated the project page text to Portuguese.

--- src/app/(public)/projects/page.tsx
+++ src/app/(public)/projects/page.tsx
@@ -1,17 +1,21 @@
 import ProjectCard from '@/features/projects/ProjectCard'
 import { mockProjects } from '@/features/projects/mocks'
+
 export default function ProjectPage() {
   return (
     <section className='w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 flex flex-col gap-12  '>
       <div className='text-center max-w-4xl mx-auto '>
         <h1 className='text-4xl md:text-6xl font-bold tracking-tight mb-6 text-primary'>
-          My Projects
+          Meus Projetos
         </h1>
+
         <p className='text-lg md:text-xl text-muted-foreground max-w-3xl mx-auto leading-relaxed'>
-          A collection of real-world projects where I explore modern
-          technologies, UI patterns, and best practices.
+          Uma coleção de projetos desenvolvidos em cenários reais, nos quais
+          exploro tecnologias modernas, padrões de interface e boas práticas
+          de desenvolvimento.
         </p>
       </div>
+
       <div className='grid  lg:grid-cols-2 justify-items-center gap-8'>
         {mockProjects.map((project) => (
           <ProjectCard key={project.projectName} {...project} />
@@ -20,3 +24,4 @@ export default function ProjectPage() {
     </section>
   )
 }
+
